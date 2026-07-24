838ae8cda017897fed82bdfadb6b65ee9c2eef1e
YaslanGlobal-Kris <yaslan.global@gmail.com>
2026-07-24 11:34:26 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@ import requests
 
 # 基础配置
 TARGET_URL = os.getenv("MY_SERVERG", "https://llcx.heeh.help")
-MAX_WORKERS = 10  # 并发线程数
+MAX_WORKERS = 30  # 并发线程数
 
 # 从 GitHub Secrets 读取敏感信息
 USER = os.getenv("MY_USER", "abcd")
